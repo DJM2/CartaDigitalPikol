79b24534801e2d21f3e25c8b3ca9e446477a522f
Djm2 <mirandadjmdjm@gmail.com>
2023-03-24 21:42:17 -0500

    Commit vierens

--- javascript.js
+++ javascript.js
@@ -7,7 +7,6 @@ function incrementar(precio, num, valor) {
     var subtotalElement = document.getElementById(precio);
     var subtotalValue = parseFloat(subtotalElement.innerHTML);
     var nuevoSubtotal = subtotalValue * numValue;
-    //agregar multiplicacion oprecio * cantidad
     var valor = document.getElementById(valor);
     valor.innerHTML = nuevoSubtotal.toFixed(2);
 }
@@ -31,6 +30,9 @@ function decrementar(precio, num, valor) {
     }
 }
 
+//Sumar
+var nTotal = 0;
+
 function sumar(precio, num, producto) {
     var sumaTotal = parseFloat(document.getElementById(precio).innerHTML);
     var cantidad = parseFloat(document.getElementById(num).value);
@@ -40,15 +42,16 @@ function sumar(precio, num, producto) {
     var izq = document.getElementById(producto).innerHTML;
     valores.innerHTML += '<br> → ' + cantidad + '(' + izq + ')' + ' = s/.' + sumaTotal.toFixed(2);
 
+    //acumular precio
     var precioTotal = parseFloat(document.getElementById('mtotal').innerHTML.replace('s/.', ''));
-
     if (isNaN(precioTotal)) {
         precioTotal = 0;
     }
     var nuevoTotal = precioTotal + sumaTotal;
     document.getElementById('mtotal').innerHTML = 's/.' + nuevoTotal.toFixed(2);
-    var nTotal = 0;
-    var nTotal = cantidad + nTotal;
+
+    // acumular cantidad en nTotal
+    nTotal += cantidad;
     document.getElementById('numeroT').innerHTML = nTotal;
 }
 
@@ -57,28 +60,11 @@ function pedido() {
     const valorespop = document.getElementById("valores-pop");
     valorespop.innerHTML = valores;
 
-    const cant = document.getElementById("numeroT").innerHTML;
+    /* const cant = document.getElementById("numeroT").innerHTML;
     const cantpop = document.getElementById("numeroT-pop");
-    cantpop.innerHTML = '<br>' + cant;
+    cantpop.innerHTML = '<br>' + cant; */
 
     const precio = document.getElementById("mtotal").innerHTML;
     const preciopop = document.getElementById("mtotal-pop");
     preciopop.innerHTML = '<br>' + precio;
 }
-
-
-
-
-
-
-/* function sumarTotal(precioId) {
-    var precio = parseFloat(document.getElementById(precioId).innerHTML);
-    var subtotal = document.getElementById('mastotal');
-
-    // Obtenemos el valor actual del subtotal, lo convertimos a número y sumamos el precio del producto actual
-    var subtotalValor = parseFloat(subtotal.innerHTML);
-    subtotalValor = subtotalValor + precio;
-
-    // Actualizamos el valor del subtotal con la suma total
-    subtotal.innerHTML = subtotalValor.toFixed(2);
-} */
