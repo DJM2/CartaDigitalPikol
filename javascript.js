4ad2e720bea59b17449d50404608b9c09168073b
Djm2 <mirandadjmdjm@gmail.com>
2023-04-03 07:25:56 -0500

    Commit Lunes

--- javascript.js
+++ javascript.js
@@ -33,28 +33,6 @@ function decrementar(precio, num, valor) {
 //Sumar
 var nTotal = 0;
 
-/* function sumar(precio, num, producto, infusiones) {
-    var sumaTotal = parseFloat(document.getElementById(precio).innerHTML);
-    var cantidad = parseFloat(document.getElementById(num).value);
-    var display = document.getElementById('display');
-    display.style.display = "block";
-    var valores = document.getElementById('valores');
-    var izq = document.getElementById(producto).innerHTML;
-    var select = document.getElementById(selectId).value;
-    valores.innerHTML += '<br> ➤ ' + cantidad + '(' + izq + select + ')' + ' = s/.' + sumaTotal.toFixed(2);
-
-    //acumular precio
-    var precioTotal = parseFloat(document.getElementById('mtotal').innerHTML.replace('s/.', ''));
-    if (isNaN(precioTotal)) {
-        precioTotal = 0;
-    }
-    var nuevoTotal = precioTotal + sumaTotal;
-    document.getElementById('mtotal').innerHTML = 's/.' + nuevoTotal.toFixed(2);
-
-    // acumular cantidad en nTotal
-    nTotal += cantidad;
-    document.getElementById('numeroT').innerHTML = nTotal;
-} */
 function sumar(precio, num, producto) {
     var sumaTotal = parseFloat(document.getElementById(precio).innerHTML);
     var cantidad = parseFloat(document.getElementById(num).value);
@@ -82,9 +60,6 @@ function sumar(precio, num, producto) {
         numeroT.classList.add('rezoom');
     }, 400);
 }
-
-
-
 
 function pedido() {
     const valores = document.getElementById("valores").innerHTML;
@@ -119,6 +94,8 @@ function wasa() {
     window.open(url);
 }
 
+//
+
 document.addEventListener("DOMContentLoaded", function() {
     updateInfusionText("zenit", "zeniti", "zenitt");
     updateInfusionText("infusion", "infusioni", "infusiont");
@@ -134,30 +111,62 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-/* document.addEventListener("DOMContentLoaded", function() {
-    updateLimonadaText("limonada", "limonadai", "limonadat");
-
-    function updateLimonadaText(limonadaId, selectId, resultId) {
-        const selectElement = document.getElementById(selectId);
-        selectElement.addEventListener("change", (event) => {
-            const infusionText = document.getElementById(limonadaId).innerHTML;
-            const resultado = document.getElementById(resultId);
-            resultado.textContent = `${infusionText} ${event.target.value}`;
-        });
-    }
-}); */
+//Refrescantes
 document.addEventListener("DOMContentLoaded", function() {
-    updateLimonadaText("limonada", "limonadai", "limonadat");
+    updateLimonadaText("limonada", "limonadai", "limonadat", "sub-limonada", "valor-limonada", 6, 15, "num-limonada");
+    updateLimonadaText("maracuya", "maracuyai", "maracuyat", "sub-maracuya", "valor-maracuya", 6, 15, "num-maracuya");
+    updateLimonadaText("morada", "moradai", "moradat", "sub-morada", "valor-morada", 6, 15, "num-morada");
+    updateLimonadaText("hierba", "hierbai", "hierbat", "sub-hierba", "valor-hierba", 8, 17, "num-hierba");
+    updateLimonadaText("limonadaf", "limonadafi", "limonadaft", "sub-limonadaf", "valor-limonadaf", 8, 18, "num-limonadaf");
+    updateLimonadaText("maracuyaf", "maracuyafi", "maracuyaft", "sub-maracuyaf", "valor-maracuyaf", 8, 18, "num-maracuyaf");
+    updateLimonadaText("moradaf", "moradafi", "moradaft", "sub-moradaf", "valor-moradaf", 8, 18, "num-moradaf");
+    updateLimonadaText("hierbaf", "hierbafi", "hierbaft", "sub-hierbaf", "valor-hierbaf", 8, 18, "num-hierbaf");
 });
 
-function updateLimonadaText(limonadaId, selectId, resultId) {
+function updateLimonadaText(limonadaId, selectId, resultId, sub, valor, precioV, precioJ, numId) {
     const selectElement = document.getElementById(selectId);
     selectElement.addEventListener("change", (event) => {
         const infusionText = document.getElementById(limonadaId).innerHTML;
-        const resultado = document.getElementById('sub-limonada');
-        const precio = (event.target.value === "Vaso") ? 6 : 15;
+        const resultado = document.getElementById(sub);
+        const valorT = document.getElementById(valor);
+        const precio = (event.target.value === "Vaso") ? precioV : precioJ;
+        const valorUnidad = parseInt(document.getElementById(numId).value);
+        const valorTotal = valorUnidad * precio;
         resultado.innerHTML = precio;
+        valorT.innerHTML = valorTotal;
         const texto = document.getElementById(resultId);
         texto.innerHTML = `${infusionText} ${event.target.value}`;
+    });
+}
+//Gaseosas
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateGasText("coca", "cocai", "cocat", "sub-coca", "valor-coca", 5, 10, 15, "num-coca");
+    updateGasText("inka", "inkai", "inkat", "sub-inka", "valor-inka", 5, 10, 15, "num-inka");
+});
+
+function updateGasText(limonadaId, selectId, resultId, sub, valor, precioV, precioJ, precioK, numId) {
+    const selectElement = document.getElementById(selectId);
+    selectElement.addEventListener("change", (event) => {
+        const infusionText = document.getElementById(limonadaId).innerHTML;
+        const resultado = document.getElementById(sub);
+        const valorT = document.getElementById(valor);
+        let precio;
+        if (event.target.value === "medioLitro") {
+            valueText = "1/2 Litro";
+            precio = precioV;
+        } else if (event.target.value === "Litro") {
+            valueText = "Litro";
+            precio = precioJ;
+        } else if (event.target.value === "2Litros") {
+            valueText = "2 Litros";
+            precio = precioK;
+        }
+        const valorUnidad = parseInt(document.getElementById(numId).value);
+        const valorTotal = valorUnidad * precio;
+        resultado.innerHTML = precio;
+        valorT.innerHTML = valorTotal;
+        const texto = document.getElementById(resultId);
+        texto.innerHTML = `${infusionText} ${valueText}`;
     });
 }
